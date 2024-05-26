4898ee61dd610f8d57d10dc23d3cbdededfa6888
YJack0000 <jackleagends@gmail.com>
2024-05-27 03:11:03 +0800

    feat: make food_path demo work

--- src/core/Environment.cpp
+++ src/core/Environment.cpp
@@ -1,8 +1,12 @@
+#include <boost/uuid/uuid_io.hpp>
 #include <core/Environment.hpp>
 #include <core/Food.hpp>
 #include <index/SpatialIndex.hpp>
+#include <iomanip>
+#include <iostream>
 #include <memory>
 #include <vector>
+#include <algorithm>
 
 Environment::Environment(int width, int height, std::string type)
     : width(width), height(height) {
@@ -33,11 +37,16 @@ void Environment::addFood(int x, int y) {
     objectsMapper[id] = food;
 }
 
-void Environment::simulateIteration(int iterations) {
+void Environment::simulateIteration(
+    int iterations, std::function<void(const Environment&)> on_each_iteration) {
     for (int i = 0; i < iterations; i++) {
         handleInteractions();
+        postIteration();
+
+        if (on_each_iteration) {
+            on_each_iteration(*this);
+        }
     }
-    postIteration();
 }
 
 void Environment::postIteration() {
@@ -45,24 +54,49 @@ void Environment::postIteration() {
     for (auto& object : objectsMapper) {
         object.second->postIteration();
     }
+
     // remove dead organisms and food
     for (auto& object : objectsMapper) {
         if (auto organism =
                 std::dynamic_pointer_cast<Organism>(object.second)) {
             if (!organism->isAlive()) {
+                std::cout << "Removing dead organism: " << object.first
+                          << std::endl;
                 spatialIndex->remove(object.first);
+                objectsMapper.erase(object.first);
             }
         } else if (auto food = std::dynamic_pointer_cast<Food>(object.second)) {
-            if (!food->canBeEaten()) {
+            if(!food->canBeEaten()) {
+                std::cout << "Removing eaten food: " << object.first << std::endl;
                 spatialIndex->remove(object.first);
+                objectsMapper.erase(object.first);
             }
         }
     }
 
-    // update position of all organisms
+    // update position of all objects
     for (auto& object : objectsMapper) {
-        spatialIndex->update(object.first, object.second->getPosition().first,
-                             object.second->getPosition().second);
+        if (auto organism =
+                std::dynamic_pointer_cast<Organism>(object.second)) {
+            // move the alive organisms
+            if (!organism->isAlive()) {
+                continue;
+            }
+
+            // add boundary check
+            std::pair<float, float> position = organism->getPosition();
+            if(position.first < 0 || position.first > width) {
+                position.first = std::max(0.0f, width < position.first ? width : position.first);
+            }
+            if(position.second < 0 || position.second > height) {
+                position.second = std::max(0.0f, height < position.second ? height : position.second);
+            }
+            organism->setPosition(position.first, position.second);
+
+            spatialIndex->update(object.first,
+                                organism->getPosition().first,
+                                organism->getPosition().second);
+        }
     }
 }
 
@@ -85,18 +119,24 @@ void Environment::handleInteractions() {
 
             std::pair<float, float> position = organism->getPosition();
 
-            // get all objects that are within the interaction radius, which is 0
+            // get all objects that are within the interaction radius, which is
             std::vector<boost::uuids::uuid> interactables =
                 spatialIndex->query(position.first, position.second, 1);
+            std::cout << "Interactables: " << interactables.size() << std::endl;
             for (auto& interactable : interactables) {
+                if(interactable == object.first) { // skip itself
+                    continue;
+                }
                 organism->interact(objectsMapper[interactable]);
             }
 
-            // get objects that are within the react radius, which is defned by genes
+            // get objects that are within the react radius, which is defned by
+            // genes
             std::vector<boost::uuids::uuid> reactables = spatialIndex->query(
                 position.first, position.second, organism->getReactionRadius());
+            std::cout << "Reactables: " << reactables.size() << std::endl;
             for (auto& reactable : reactables) {
-                if (reactable == object.first) {
+                if (reactable == object.first) { // skip itself
                     continue;
                 }
                 // react with the object pointer from objectMapper
@@ -106,10 +146,32 @@ void Environment::handleInteractions() {
     }
 }
 
-std::vector<std::shared_ptr<BaseEnvironmentObject>> Environment::getAllObjects() const {
-        std::vector<std::shared_ptr<BaseEnvironmentObject>> objects;
-        for (const auto& object : objectsMapper) {
-            objects.push_back(object.second);
+std::vector<std::shared_ptr<BaseEnvironmentObject>> Environment::getAllObjects()
+    const {
+    std::cout << "\n===================\n";
+    std::cout << "Getting all objects" << std::endl;
+    std::vector<std::shared_ptr<BaseEnvironmentObject>> objects;
+    for (const auto& object : objectsMapper) {
+        if (auto organism =
+                std::dynamic_pointer_cast<Organism>(object.second)) {
+            std::cout << std::left << std::setw(10)
+                      << "Organism: " << std::setw(38) << object.first
+                      << std::setw(4) << (int)object.second->getPosition().first
+                      << std::setw(4)
+                      << (int)object.second->getPosition().second << " "
+                      << std::setw(6)
+                      << (organism->isAlive() ? "Alive" : "Dead")
+                      << std::setw(10) << organism->getSpeed() << std::setw(10)
+                      << organism->getSize() << std::setw(10)
+                      << organism->getAwareness() << std::endl;
+        } else if (auto food = std::dynamic_pointer_cast<Food>(object.second)) {
+            std::cout << std::left << std::setw(10) << "Food: " << std::setw(38)
+                      << object.first << std::setw(4)
+                      << object.second->getPosition().first << std::setw(4)
+                      << object.second->getPosition().second << " "
+                      << (food->canBeEaten() ? "fresh" : "eaten") << std::endl;
         }
-        return objects;
+        objects.push_back(object.second);
     }
+    return objects;
+}
