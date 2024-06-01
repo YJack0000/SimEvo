8f9fd7dec400d542cf03045986d3fb0da57147ad
YJack0000 <jackleagends@gmail.com>
2024-06-01 18:11:59 +0800

    feat: add doxygen comment on OptimizedSpatailIndex

--- src/index/OptimizedSpatialIndex.cpp
+++ src/index/OptimizedSpatialIndex.cpp
@@ -9,9 +9,20 @@ const int OptimizedSpatialIndex<T>::MAX_OBJECTS = 10;
 template <typename T>
 const int OptimizedSpatialIndex<T>::MIN_SIZE = 10;
 
+/**
+ * @brief Constructs a new instance of OptimizedSpatialIndex with a specified size.
+ * @param size The size of the index area.
+ */
 template <typename T>
 OptimizedSpatialIndex<T>::OptimizedSpatialIndex(int size) : size(size), isSubdivided(false) {}
 
+/**
+ * @brief Inserts a new object into the spatial index.
+ *
+ * @param object The object to insert.
+ * @param x The x-coordinate of the object.
+ * @param y The y-coordinate of the object.
+ */
 template <typename T>
 void OptimizedSpatialIndex<T>::insert(const T &object, float x, float y) {
     if (!inBounds(std::make_pair(x, y))) {
@@ -26,6 +37,14 @@ void OptimizedSpatialIndex<T>::insert(const T &object, float x, float y) {
     this->addObjectPositionPair(object, x, y);
 }
 
+/**
+ * @brief Queries the spatial index for objects within a specified range.
+ *
+ * @param x The x-coordinate of the query center.
+ * @param y The y-coordinate of the query center.
+ * @param range The query radius.
+ * @return std::vector<T> A list of objects within the range.
+ */
 template <typename T>
 std::vector<T> OptimizedSpatialIndex<T>::query(float x, float y, float range) {
     std::vector<T> result;
@@ -49,6 +68,13 @@ std::vector<T> OptimizedSpatialIndex<T>::query(float x, float y, float range) {
     return result;
 }
 
+/**
+ * @brief Updates the position of an object in the spatial index.
+ *
+ * @param object The object to update.
+ * @param newX The new x-coordinate of the object.
+ * @param newY The new y-coordinate of the object.
+ */
 template <typename T>
 void OptimizedSpatialIndex<T>::update(const T &object, float newX, float newY) {
     remove(object);
@@ -58,6 +84,11 @@ void OptimizedSpatialIndex<T>::update(const T &object, float newX, float newY) {
     this->addObjectPositionPair(object, newX, newY);
 }
 
+/**
+ * @brief Removes an object from the spatial index.
+ *
+ * @param object The object to remove.
+ */
 template <typename T>
 void OptimizedSpatialIndex<T>::remove(const T &object) {
     auto it =
@@ -85,17 +116,32 @@ void OptimizedSpatialIndex<T>::remove(const T &object) {
 template <typename T>
 void OptimizedSpatialIndex<T>::clear() {
     spatialObjects.clear();
+    
+    // Clear each child and then reset the unique_ptr
     for (auto &child : children) {
-        child.reset();
+        if (child) {
+            child->clear();  // Ensure child clears its own resources
+            child.reset();   // Release the object and set pointer to nullptr
+        }
     }
+
     this->clearObjectPositionPairs();
 }
 
+/**
+ * @brief Helper function to check if a position is within the bounds of the spatial index.
+ *
+ * @param pos A pair of float representing the x and y coordinates.
+ * @return true If the position is within bounds.
+ */
 template <typename T>
 bool OptimizedSpatialIndex<T>::inBounds(const std::pair<float, float> &pos) {
     return pos.first >= 0 && pos.first < size && pos.second >= 0 && pos.second < size;
 }
 
+/**
+ * @brief Subdivides the spatial index into smaller indices.
+ */
 template <typename T>
 void OptimizedSpatialIndex<T>::subdivide() {
     int childSize = size / 2;
@@ -123,35 +169,66 @@ void OptimizedSpatialIndex<T>::subdivide() {
     isSubdivided = true;
 }
 
+/**
+ * @brief Sets the offset for this spatial index, used when subdivided.
+ *
+ * @param offsetX The offset in the x-coordinate.
+ * @param offsetY The offset in the y-coordinate.
+ */
 template <typename T>
 void OptimizedSpatialIndex<T>::setOffset(float offsetX, float offsetY) {
-    offset = {offsetX, offsetY};
+    offset = std::make_pair(offsetX, offsetY);
 }
 
+/**
+ * @brief Checks if it is possible to merge children back into this node.
+ *
+ * @return true If all children are empty and the subdivision can be reversed.
+ */
 template <typename T>
 bool OptimizedSpatialIndex<T>::canMerge() const {
     if (!isSubdivided) {
         return false;
     }
     for (const auto &child : children) {
-        if (!child->isEmpty()) {
+        if (child && !child->isEmpty()) {
             return false;
         }
     }
     return true;
 }
 
+/**
+ * @brief Merges all children back into this node, assuming they are all empty.
+ */
 template <typename T>
 void OptimizedSpatialIndex<T>::merge() {
-    isSubdivided = false;
-    for (auto &child : children) {
-        child.reset();
+    if (canMerge()) {
+        for (auto &child : children) {
+            child.reset();
+        }
+        isSubdivided = false;
     }
 }
 
+/**
+ * @brief Checks if this spatial index is empty, meaning it has no objects and no active subdivisions.
+ *
+ * @return true If there are no spatial objects and it is not subdivided.
+ */
 template <typename T>
 bool OptimizedSpatialIndex<T>::isEmpty() const {
-    return spatialObjects.empty() && !isSubdivided;
+    if (!spatialObjects.empty()) {
+        return false;
+    }
+    if (isSubdivided) {
+        for (const auto &child : children) {
+            if (child && !child->isEmpty()) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 // make sure to instantiate the template class
@@ -159,5 +236,4 @@ template class OptimizedSpatialIndex<int>;
 template class OptimizedSpatialIndex<float>;
 template class OptimizedSpatialIndex<double>;
 template class OptimizedSpatialIndex<std::string>;
-
 template class OptimizedSpatialIndex<boost::uuids::uuid>;
