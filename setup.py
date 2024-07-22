92f825498f6048a3c4ff8d67cf74aaae548aff56
YJack0000 <jackleagends@gmail.com>
2024-07-23 01:31:56 +0800

    fix: sdist install not working problem

--- setup.py
+++ setup.py
@@ -1,5 +1,36 @@
-from skbuild import setup
-from setuptools import find_packages
+import os
+import subprocess
+import sys
+from setuptools import setup, Extension, find_packages
+from setuptools.command.install import install
+from setuptools.command.build_ext import build_ext
+
+class CMakeBuild(build_ext):
+    def run(self):
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
+            f"-DPYTHON_EXECUTABLE={sys.executable}"
+        ]
+        build_temp = self.build_temp
+        if not os.path.exists(build_temp):
+            os.makedirs(build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=build_temp)
+        subprocess.check_call(['cmake', '--build', '.'], cwd=build_temp)
+
+class InstallWithCMake(install):
+    def run(self):
+        self.run_command('build_ext')
+        install.run(self)
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        super().__init__(name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -12,6 +43,8 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(),
     cmake_source_dir=".",
+    ext_modules=[CMakeExtension('simevopy')],
+    cmdclass=dict(build_ext=CMakeBuild, install=InstallWithCMake),
     url='https://github.com/YJack0000/SimEvo',
     author='YJack0000',
     author_email='yjack0000.cs12@nycu.edu.tw',
